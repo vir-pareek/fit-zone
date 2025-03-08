1b7ae9bd8bb6803b3491ace83ba14e544f6270d8
Vir Pareek <156666178+vir-pareek@users.noreply.github.com>
2025-03-08 10:40:40 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
                 <p>IT'S TIME TO GET</p>
                 <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Fit<span className='text-blue-400'>normous</span></h1>
             </div>
-            <p className='text-sm md:text-base font-light'>I hereby acknowledgement that I may become <span className='text-blue-400 font-medium'>incredibly fitnormous</span> and embrace all possibility of becoming the local <span className='text-blue-400 font-medium'>fitness legend</span>. I accept all risks of outgrowing my clothes, turning sideways to fit through doors, and developing an unshakable commitment to the gym life!</p>
+            <p className='text-sm md:text-base font-light'>I hereby acknowledge that I may become <span className='text-blue-400 font-medium'>incredibly fitnormous</span> and embrace the opportunity of becoming the local <span className='text-blue-400 font-medium'>fitness legend</span>. I accept all risks of outgrowing my clothes, turning sideways to fit through doors, and developing an unshakable commitment to the gym life!</p>
             <Button func={() => {
                 window.location.href = '#generate'
             }} text={"Accept & Begin"}></Button>
